d37fcf6ec6a6c66e19ebd4583cb1dc11a34d0df1
Javohir-devloper <elmurodovjavohir1998@gmail.com>
2021-08-24 17:12:15 +0500

    test

--- README.md
+++
@@ -1,7 +0,0 @@
-commamds:
-
-
-php index.php sms
-
-
-php index.php poch
--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php
-require('service/Sms.php');
-require('service/Poch.php');
-use service\Sms;
-use service\Poch;
-
-class server{
-    public function __construct($argv){
-        switch ($argv[1]) {
-            case 'sms':
-                new Sms();
-                break;
-            case 'poch':
-                new Poch();
-                break;
-            default:
-                $error = "Ошибка: такой команды не существует". PHP_EOL;
-                echo $error;
-                $filename = "error.txt";
-                $fh = fopen($filename, "w");
-                fwrite($fh, $error);
-                fclose($fh);
-        }
+echo strrev("Hello world!"). PHP_EOL;
+function mb_strrev($str){
+    $r = '';
+    for ($i = mb_strlen($str); $i>=0; $i--) {
+        $r .= mb_substr($str, $i, 1);
     }
-
+    return $r. PHP_EOL;
 }
-new server($argv);
+
+echo mb_strrev("world"). PHP_EOL;
+
+$string = 'This is manoj';
+$n =strlen("$string");
+For($i=1;$i<=$n;$i++)
+{
+    $val= $string[-$i];
+    echo $val;
+}
+?>
